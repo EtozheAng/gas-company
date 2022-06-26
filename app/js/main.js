18f3b7e203f24d1e09e8934dcd780bd906a76b46
EtozheAng <pavel.khisamutdinov@yandex.ru>
2022-06-26 17:01:08 +0300

    fix tabs

--- app/js/main.js
+++ app/js/main.js
@@ -1,15 +1,5 @@
  $(function(){
 
-   $('.tabs__top-item').on('click', function (e) {
-     e.preventDefault();
-     $('.tabs__top-item').removeClass('tabs__top-item--active');
-     $(this).addClass('tabs__top-item--active');
-
-     $('.tabs__content-item').removeClass('tabs__content-item--active');
-     $($(this).attr('href')).addClass('tabs__content-item--active');
-
-   });
-
    $('.slider__inner').slick({
      prevArrow: '<button type="button" class="slick-prev"><img src="images/slider/arrow-prev.svg" alt="arrow-left"></button>',
      nextArrow: '<button type="button" class="slick-next"><img src="images/slider/arrow-next.svg" alt="arrow-right"></button>',
@@ -24,4 +14,6 @@
      $(this).children('.question__answer').slideToggle(400);
    });
 
- });
+   
+  });
+  
